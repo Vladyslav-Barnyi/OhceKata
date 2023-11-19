a26701011abb54f6686ddcf4c3ac5810c6e0ccc8
Vladyslav Barnyi <vladyslavbarnyi@gmail.com>
2023-11-19 22:06:29 +0100

    UPDATE.2.2

--- OhceTests/GreetingServiceTests.cs
+++ OhceTests/GreetingServiceTests.cs
@@ -1,5 +1,7 @@
 using FluentAssertions;
+using NSubstitute;
 using Ohce.Services;
+using Ohce.Services.Interfaces;
 
 namespace OhceTests;
 
@@ -21,7 +23,7 @@ public class GreetingServiceTests
     }
 
     [Fact]
-    public void GreetUser_ReturnsMorningGreeting_WhenAfternoon()
+    public void GreetUser_ReturnsAfternoonGreeting_WhenAfternoon()
     {
         var morningTime = new DateTime(2023, 1, 1, 15, 0, 0);
         var result = _greetingService.GreetUser("Vlad", morningTime);
@@ -29,10 +31,46 @@ public class GreetingServiceTests
     }
     
     [Fact]
-    public void GreetUser_ReturnsMorningGreeting_WhenEvening()
+    public void GreetUser_ReturnsEveningGreeting_WhenEvening()
     {
         var morningTime = new DateTime(2023, 1, 1, 22, 0, 0);
         var result = _greetingService.GreetUser("Vlad", morningTime);
+        result.Should().BeEquivalentTo("¡Buenas noches Vlad!");
+    }
+    
+    
+    [Fact]
+    public void SubstituteGreetUser_ReturnsMorningGreeting_WhenMorning()
+    {
+        var morningTime = new DateTime(2023, 1, 1, 9, 0, 0);
+        var sut = Substitute.For<IGreetingService>();
+        sut.GreetUser("Vlad", morningTime).Returns("¡Buenas días Vlad!");
+        
+        var result = sut.GreetUser("Vlad", morningTime);
+        result.Should().Be("¡Buenos días Vlad!");
+    }
+
+    [Fact]
+    public void SubstituteGreetUser_ReturnsAfternoonGreeting_WhenAfternoon()
+    {
+        var afternoonTime = new DateTime(2023, 1, 1, 15, 0, 0);
+        var sut = Substitute.For<IGreetingService>();
+        sut.GreetUser("Vlad", afternoonTime).Returns("¡Buenas tardes Vlad!");
+        
+        var result = sut.GreetUser("Vlad", afternoonTime);
+        result.Should().Be("¡Buenas tardes Vlad!");
+    }
+    
+    [Fact]
+    public void SubstituteGreetUser_ReturnsEveningGreeting_WhenEvening()
+    {
+        var eveningTime = new DateTime(2023, 1, 1, 22, 0, 0);
+        var sut = Substitute.For<IGreetingService>();
+
+        sut.GreetUser("Vlad", eveningTime).Returns("¡Buenas noches Vlad!");
+
+        var result = sut.GreetUser("Vlad", eveningTime);
+        
         result.Should().BeEquivalentTo("¡Buenas noches Vlad!");
     }
 }
--- OhceTests/ReverseEchoingServiceTests.cs
+++ OhceTests/ReverseEchoingServiceTests.cs
@@ -1,5 +1,7 @@
 using FluentAssertions;
+using NSubstitute;
 using Ohce.Services;
+using Ohce.Services.Interfaces;
 
 namespace OhceTests;
 
@@ -23,6 +25,26 @@ public class ReverseEchoingServiceTests
     public void RevertString_ReturnsPalindromeMessage_WhenPalindrome()
     {
         var result = _reverseEchoingService.RevertString("radar");
+        result.Should().Be("radar\n¡Bonita palabra!");
+    }
+    
+    [Fact]
+    public void SubstituteRevertString_ReturnsReversedString_WhenNotPalindrome()
+    {
+        var reverseEchoingService = Substitute.For<IReverseEchoingService>();
+        reverseEchoingService.RevertString("hello").Returns("olleh");
+        
+        var result = reverseEchoingService.RevertString("hello");
+        result.Should().Be("olleh");
+    }
+
+    [Fact]
+    public void SubstituteRevertString_ReturnsPalindromeMessage_WhenPalindrome()
+    {
+        var reverseEchoingService = Substitute.For<IReverseEchoingService>();
+        reverseEchoingService.RevertString("radar").Returns("radar\n¡Bonita palabra!");
+
+        var result = reverseEchoingService.RevertString("radar");
         result.Should().Be("radar\n¡Bonita palabra!");
     }
 }
